e42de21468d48d149d746ebb4069e6728838e581
RDuke-Cantina <60246850+RDuke-Cantina@users.noreply.github.com>
2020-01-23 22:22:36 -0500

    Hello World
    a button and title

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Randolph Duke on 1/17/20.
+//  Copyright © 2020 Randolph Duke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
